99e947b5b71fc7cfe2f7919ec88fefa41642d91a
askararu <ar_askar@kbtu.kz>
2026-01-29 21:52:23 +0500

    lol

---
+++ practice1/casting.py
@@ -0,0 +1,15 @@
+#integers
+x = int(3) # it is 1
+y = int(3.8) #it is 3
+z = int("5") # it is 5
+ 
+#floats
+x = float(3)     # it is 3.0
+y = float(2.5)   # it is 2.5
+z = float("5")   # it is 5.0
+w = float("7.5") # it is  7.5
+
+#strings
+x = str("aru05") # it is 'aru05'
+y = str(2)    # y will be '2'
+z = str(3.0)  # z will be '3.0'
