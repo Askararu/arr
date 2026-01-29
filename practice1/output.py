826bbf8c801c2c71bcdbfafc09ae8211da9d18ee
askararu <ar_askar@kbtu.kz>
2026-01-29 19:58:51 +0500

    k

---
+++ practice1/output.py
@@ -0,0 +1,11 @@
+#we can use the print() function as many times as we want. 
+print("Hello!!!")
+print("I am Aru")
+
+#we can use either " double quotes or ' single quotes
+print("It will work")
+print('it also will work')
+
+#If we want to print multiple words on the same line, we can use the end parameter
+print("Hello", end=" ")
+print("world!!!")
