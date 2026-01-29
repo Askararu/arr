015baa6908700a1e115554f1fd0becba3f778598
askararu <ar_askar@kbtu.kz>
2026-01-29 13:23:10 +0500

    a

---
+++ practice1/comment.py
@@ -0,0 +1,15 @@
+#this is a comment
+print("Hello,world")
+print("It is funny") # That is also a comment 
+#print("I am writing a comment")
+print("i am writing a comment")
+#I just wrote
+#a comment in 
+#more than just one line
+print("Hello, everyonee")
+"""
+This  another way
+of writig 
+a comment
+"""
+print("I did it ")
