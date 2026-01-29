8a0b607f6a09ee5514e0ff1b3985be1339f0a8ab
askararu <ar_askar@kbtu.kz>
2026-01-29 21:22:42 +0500

    kkk

---
+++ practice1/datatypes.py
@@ -0,0 +1,55 @@
+#str 
+x = str("i love chocolate") 
+print(x)
+
+#int
+x = int(17)
+print(x)
+
+#float
+x = float(15.7)
+print(x)
+ 
+#complex
+x = complex(3j)
+print(x)
+
+#list
+x = list(("pink","white","black"))
+print(x)
+
+#tuple
+x = tuple(("sweet","salty","spicy"))
+print(x)
+
+#range
+x = range(7)
+print(x)
+
+#dict
+x = dict(name="Aru",age=17)
+print(x)
+
+#set
+x = set(("Dayana", "Aru", "Balnur"))
+print(x)
+
+#frozenset
+x = frozenset(("strawberry", "berry", "cherry"))
+print(x)
+
+#bool
+x = bool(3)
+print(x)
+
+#bytes
+x =bytes(5)
+print(x)
+
+#bytearray
+x = bytearray(7)
+print(x)
+
+#memoryview
+x = memoryview(bytes(5))
+print(x)
