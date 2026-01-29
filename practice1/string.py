7b8d4d9c89bba56c714bf96c3eae2f500ef9419c
askararu <ar_askar@kbtu.kz>
2026-01-29 22:06:11 +0500

    so

---
+++ practice1/string.py
@@ -0,0 +1,19 @@
+#Assigning a string to a variable
+a = "chocolatemilk" 
+print(a)
+
+#Strings are Arrays
+a = "love"
+print(a[1])
+
+#looping
+for x in "strawberrymilk":
+  print(x)
+
+#slicing
+a = "bananmilk"
+print(a[2:5])
+
+#upper case
+a = "milk"
+print(a.upper())
