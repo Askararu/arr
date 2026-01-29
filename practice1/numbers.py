60033e6a8f55834c9dc6adbe940f75f447383265
askararu <ar_askar@kbtu.kz>
2026-01-29 21:36:00 +0500

    enl

---
+++ practice1/numbers.py
@@ -0,0 +1,30 @@
+#int floats complex 
+x = 5
+y = 6.9
+z = 3j
+print(type(x))
+print(type(y))
+print(type(z))
+
+#convert from one type to another
+x = 1    
+y = 5.5
+z = 27j
+
+a = float(x)
+
+b = int(y)
+
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
+ 
+#random number 
+import random
+print(random.randrange(3,17))
