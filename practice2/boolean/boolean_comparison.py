59c3809d9928ac14e6fe2482fae61fa3d712c5ab
askararu <ar_askar@kbtu.kz>
2026-02-05 22:15:30 +0500

    examples of boolean

---
+++ practice2/boolean/boolean_comparison.py
@@ -0,0 +1,15 @@
+#1.Comparing numbers
+print(5 > 2)        
+#2.Checking equality
+print(7 == 3)       
+#3.Comparing variables
+a = 4
+b = 4
+print(a == b)       
+#4.Checking inequality
+x = 6
+y = 6
+print(x != y)       
+
+#5.Greater than or equal
+print(10 >= 8) 
---
+++ practice2/boolean/boolean_operators.py
@@ -0,0 +1,25 @@
+
+#1.AND operator
+a = True
+b = False
+print(a and b)  
+
+#2.OR operator 
+x = True
+y = False
+print(x or y)    
+
+#3.NOT operator 
+cake = True
+print(not cake)  
+
+#4.Combining AND and OR
+age = 21
+energydrink = True
+canbuy= (age >= 21) and energydrink
+print(canbuy) 
+
+#5.Using NOT with a comparison
+temperature = 30
+cold = not (temperature > 25)
+print(cold)   
