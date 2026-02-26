3617127abee000b577390c1b387a3f7e30d11710
askararu <ar_askar@kbtu.kz>
2026-02-26 23:49:38 +0500

    Add Practice4 - Python iterators, generators, dates, math, and JSON

--- practice4/JSON.py
+++ practice4/JSON.py
@@ -1,22 +1,17 @@
 import json
-
-# open JSON file and load data
-with open("sample-data.json") as f:
-    data = json.load(f)
+with open('sample-data.json', 'r') as file:
+    data = json.load(file)
 
 print("Interface Status")
 print("=" * 80)
 print(f"{'DN':50} {'Description':20} {'Speed':8} {'MTU':6}")
-print("-" * 50 + " " + "-" * 20 + " " + "-" * 8 + " " + "-" * 6)
-
-# go through all interfaces inside imdata
+print("-" * 80)
 for item in data["imdata"]:
-    attrs = item["l1PhysIf"]["attributes"]
-
-    dn = attrs.get("dn", "")
-    descr = attrs.get("descr", "")
-    speed = attrs.get("speed", "")
-    mtu = attrs.get("mtu", "")
-
-    # print formatted row
+    attributes = item["l1PhysIf"]["attributes"]
+    
+    dn = attributes.get("dn", "")
+    descr = attributes.get("descr", "")
+    speed = attributes.get("speed", "")
+    mtu = attributes.get("mtu", "")
+    
     print(f"{dn:50} {descr:20} {speed:8} {mtu:6}")
